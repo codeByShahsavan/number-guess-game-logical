8415f2a0a633a26d4ba233ea711025fe423ff780
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-03 15:09:35 +0330

    validation

--- app.js
+++ app.js
@@ -2,13 +2,28 @@
 const randomNumber=Math.ceil(Math.random()*100)
 console.log(randomNumber)
 
+const validateNumber=value=>{
+     if(isNaN(value)){
+      return "please enter a valid number"
+     }else if(+value<1 || +value>100){
+      return "please enter a number between 1 to 100"
+     }
+}
+
 const play=()=>{
   const number=prompt("Enter a number between 1 to 100")
 
-  if(number===null){
-    return
+  if(number===null) return
+
+
+  const validation=validateNumber(number)
+  console.log(validation)
+  if(validation){
+    return play()
   }
-    console.log(number)
 }
+
+
+
 
 play()
