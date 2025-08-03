0c55f75fc657fba6f53f0b2abc04d2866349e788
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-03 16:28:00 +0330

    limit the number of game rounds

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const randomNumber=Math.ceil(Math.random()*100)
 console.log(randomNumber)
-
+let numberOfGuess=0
 const validateNumber=value=>{
      if(isNaN(value)){
       return "please enter a valid number"
@@ -11,15 +11,19 @@ const validateNumber=value=>{
 }
 
 const checkGuess=(guess)=>{
+  numberOfGuess++
+    console.log(numberOfGuess)
+
    if(guess===randomNumber){
     console.log("You guessed correctly")
    }
     else if(guess<randomNumber){
     console.log("Too low!Try again")
-   
+   play()
    }
    else {
     console.log("Too high!Try again")
+    play()
    }
 }
 
@@ -27,7 +31,12 @@ const play=()=>{
   const number=prompt("Enter a number between 1 to 100")
 
   if(number===null) return
+   
 
+  if(numberOfGuess>=10){
+    console.log("Game over")
+    return
+  }
 
   const validation=validateNumber(number)
 
