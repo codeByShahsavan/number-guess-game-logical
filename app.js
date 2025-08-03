93bf56c5cd850044c5ca8a1ef9aab25aa5991cd6
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-08-03 13:03:46 +0330

    get a number from user

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+const randomNumber=Math.ceil(Math.random()*100)
+console.log(randomNumber)
+
+const play=()=>{
+  const number=prompt("Enter a number between 1 to 100")
+
+  if(number===null){
+    return
+  }
+    console.log(number)
+}
+
+play()
